397d597ff6e147b5cb5736f7ed7be5d32e1a275a
gokulnair2001 <gokulnair.2001@gmail.com>
2021-11-27 12:50:40 +0530

    initial commit

---
+++ Falcon/FalconApp.swift
@@ -0,0 +1,17 @@
+//
+//  FalconApp.swift
+//  Falcon
+//
+//  Created by Gokul Nair on 27/11/21.
+//
+
+import SwiftUI
+
+@main
+struct FalconApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
